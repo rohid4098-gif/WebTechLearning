8127ebd81d7f683f02f42dc6a38b81c12ff26f3c
rohid4098-gif <rohid4098@gmail.com>
2026-04-12 14:12:15 +0600

    Final term commit

---
+++ Final Term/Day-1/Final Lab Task-1/a/handler.php
@@ -0,0 +1,17 @@
+<?php
+
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+
+    if ($name == "") {
+        echo "Name is empty!";
+    } else {
+        echo "Hello, " ($name);
+    }
+} else {
+    echo "Please submit the form first.";
+}
+
+
+?>
